9d3f88dc71a3e4dc878e9ab5b58479408ee3a5ec
mahesh <mahe0211@users.noreply.github.com>
2018-09-09 17:43:35 +0530

    Update format_zipcode.py

--- format_zipcode.py
+++ format_zipcode.py
@@ -1,7 +1,14 @@
+# Given the below function, please describe test scenarios for the below function.
+# NOTE: When describing your scenarios, it's OK to use simple scenario descriptions
+# for your tests. def format_zipcode(zip_code): if len(zip_code) <= 5: return
+# '{:>05}'.format(zip_code) if len(zip_code) == 10: return str(zip_code) if len(zip_code)
+# == 9: return '{}-{}'.format(zip_code[:5], zip_code[5:])
+
+
 def format_zipcode(zip_code):
     """
-    Function to format zip code based on length
-    :param str zip_code: zip code
+    Function to return zip code in based on lenghth
+    :param str zip_code: zip code to be returned
     :return: result of function
     :rtype: str
     """
@@ -21,3 +28,4 @@ print(format_zipcode('12345'))
 print(format_zipcode('1234567890'))
 print(format_zipcode('123456789'))
 print(format_zipcode('1234'))
+
